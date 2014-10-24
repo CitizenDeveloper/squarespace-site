80a164074793d98d4ebcf49b2a30019f985aa921
Nathaniel Wroblewski <NathanielWroblewski@gmail.com>
2014-10-23 23:14:04 -0700

    wip

---
+++ scripts/navbar.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  var activity = window.location.pathname.match(/activity-(.*)-/)
+  if (activity) {
+    var module = activity[1]
+    $('#moduleNav ul li:first-child() a').each(function(index, el){
+      if (index === 0) {
+        $(el).text('Modules')
+      } else {
+        $(el).text(index)
+      }
+    })
+
+  }
+})
