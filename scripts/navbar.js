481e89d2efe775f522b7b507818a4508913d58af
Nathaniel Wroblewski <NathanielWroblewski@gmail.com>
2014-10-26 20:22:36 -0700

    show navigation only on module and activity pages

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,15 +1,6 @@
 $(document).ready(function() {
-  // var activity = window.location.pathname.match(/activity-(.*)-/)
-  // if (activity) {
-  //   var module = activity[1]
-  //   $('#moduleNav ul li:first-child() a').each(function(index, el){
-  //     if (index === 0) {
-  //       $(el).html('<h2>MODULE</h2>')
-  //     } else {
-  //       $(el).text(index)
-  //       if (Number(module) === index) $(el).addClass('active-module')
-  //     }
-  //   })
-  //   $('#moduleNav').show()
-  // }
+  var address = window.location.pathname
+  if (address.match(/activity/) || address.match(/module/)) {
+    $('#activityNav').show()
+  }
 })
