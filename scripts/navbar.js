2d5ffff9f50c2e70fb90f187b8daccad65079dc8
Nathaniel Wroblewski <NathanielWroblewski@gmail.com>
2014-10-26 20:34:52 -0700

    wip

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   var address = window.location.pathname
   if (address.match(/activity/) || address.match(/module/)) {
-    $('#activityNav').show()
+    $('#activityNav').show();
+    $('#activityNav').css('display', 'inline-block');
   }
   if (address.match(/module/)) {
     var module = address.match(/module-(.*)\/content/)[1]
