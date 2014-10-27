4f8b03de59dd13fb618a569d4d3953288db894ae
Nathaniel Wroblewski <NathanielWroblewski@gmail.com>
2014-10-26 17:10:59 -0700

    wip

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-  var activity = window.location.pathname.match(/activity-(.*)-/)
-  if (activity) {
-    var module = activity[1]
-    $('#moduleNav ul li:first-child() a').each(function(index, el){
-      if (index === 0) {
-        $(el).html('<h2>MODULE</h2>')
-      } else {
-        $(el).text(index)
-        if (Number(module) === index) $(el).addClass('active-module')
-      }
-    })
-    $('#moduleNav').show()
-  }
+  // var activity = window.location.pathname.match(/activity-(.*)-/)
+  // if (activity) {
+  //   var module = activity[1]
+  //   $('#moduleNav ul li:first-child() a').each(function(index, el){
+  //     if (index === 0) {
+  //       $(el).html('<h2>MODULE</h2>')
+  //     } else {
+  //       $(el).text(index)
+  //       if (Number(module) === index) $(el).addClass('active-module')
+  //     }
+  //   })
+  //   $('#moduleNav').show()
+  // }
 })
